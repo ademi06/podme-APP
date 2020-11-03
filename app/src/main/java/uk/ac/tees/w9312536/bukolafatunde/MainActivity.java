90136a3c728799540d2bfb35052c5b1f7e4b45ff
Bukola <bukola.tees@gmail.com>
2020-11-03 16:19:02 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.w9312536.bukolafatunde;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

461aa3e7ec0c7552c6a613375bf7a11cd6464426
Bukola <bukola.tees@gmail.com>
2020-11-12 13:04:34 +0000

    rough sketch of login and signup

--- app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/ForgotPasswordActivity.java
+++ app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/ForgotPasswordActivity.java
@@ -2,13 +2,32 @@ package uk.ac.tees.w9312536.bukolafatunde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    Button resetPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        etEmail = findViewById(R.id.editTextEmail);
+        resetPasswordButton = findViewById(R.id.resetPasswordButton);
+
+        resetPasswordButton.setOnClickListener(v -> {
+            /* reset password logic */
+            String email = etEmail.getText().toString().trim();
+            Toast.makeText(getApplicationContext(), email, Toast.LENGTH_SHORT).show();
+
+        });
+
+
     }
 }
--- app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/SignUpActivity.java
+++ app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/SignUpActivity.java
@@ -2,13 +2,35 @@ package uk.ac.tees.w9312536.bukolafatunde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    TextView login;
+    EditText  etName, etEmail, etPassword;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activty_sign_up);
+        etName = findViewById(R.id.editTextName);
+        etEmail = findViewById(R.id.editTextEmail);
+        etPassword = findViewById(R.id.editTextPassword);
+        signUpButton = findViewById(R.id.signUpButton);
+        login = findViewById(R.id.tvLogin);
+
+
+        login.setOnClickListener(v -> {
+            /* navigate to login activity */
+            Intent login = new Intent(SignUpActivity.this, LoginActivity.class);
+            startActivity(login);
+            finish();
+        });
     }
+
 }

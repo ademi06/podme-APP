01c5b6b452ebc9dc8170d625afbe942e5a249644
Bukola <bukola.tees@gmail.com>
2020-12-08 21:37:49 +0000

    added edit profile ui

--- app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/ui/add/AddPodcastAdapter.java
+++ app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/ui/add/AddPodcastAdapter.java
@@ -1,4 +1,86 @@
 package uk.ac.tees.w9312536.bukolafatunde.ui.add;
 
-public class AddPodcastAdapter {
-}
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import uk.ac.tees.w9312536.bukolafatunde.R;
+import uk.ac.tees.w9312536.bukolafatunde.model.Result;
+
+//public class AddPodcastAdapter extends RecyclerView.Adapter<AddPodcastAdapter.AddPodcastViewHolder> {
+//
+//    /* Member variable for the list of {@link Result} */
+//    private List<Result> mResults;
+//
+//    // An onClick handler that we have defined to make it easy for an Activity to interface with
+//    // our RecyclerView
+//    private final AddPodcastAdapterOnClickHandler mOnClickHander;
+//
+//    @NonNull
+//    @Override
+//    public AddPodcastAdapter.AddPodcastViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+//        AddPodListItemBinding addPodListItemBinding = DataBindingUtil.inflate(inflater, R.layout.add_pod_list_item, parent, false);
+//        //View view = inflater.inflate(R.layout.add_pod_list_item, parent, false);
+//
+//        return new AddPodcastViewHolder(addPodListItemBinding);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull AddPodcastAdapter.AddPodcastViewHolder holder, int position) {
+//        Result result = mResults.get(position);
+//        holder.bind(result);
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        if (mResults == null) return 0;
+//        return mResults.size();
+//    }
+//
+//    public void addAll(List<Result> results){
+//        mResults.clear();
+//        mResults.addAll(results);
+//        notifyDataSetChanged();
+//    }
+//
+//    /* The interface that receives onClick messages */
+//    public interface AddPodcastAdapterOnClickHandler {
+//        void onItemClick(Result result, ImageView imageView);
+//    }
+//
+//    public AddPodcastAdapter(List<Result> results, AddPodcastAdapterOnClickHandler onClickHandler){
+//        mResults = results;
+//        mOnClickHander = onClickHandler;
+//    }
+//
+//
+//    public class AddPodcastViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//
+//        private AddPodListItemBinding mAddPodListItemBinding;
+//
+//        public AddPodcastViewHolder(AddPodListItemBinding addPodListItemBinding) {
+//            super(addPodListItemBinding.getRoot());
+//            mAddPodListItemBinding = addPodListItemBinding;
+//
+//                    itemView.setOnClickListener(this);
+//        }
+//
+//
+//        @Override
+//        public void onClick(View v) {
+//
+//            int adapterPosition = getBindingAdapterPosition();
+//            Result result = mResults.get(adapterPosition);
+//            mOnClickHander.onItemClick(result, mAddPodListItemBinding);
+//        }
+//    }
+//}
--- app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/ui/editprofile/EditProfileFragment.java
+++ app/src/main/java/uk/ac/tees/w9312536/bukolafatunde/ui/editprofile/EditProfileFragment.java
@@ -1,19 +1,37 @@
 package uk.ac.tees.w9312536.bukolafatunde.ui.editprofile;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import pub.devrel.easypermissions.EasyPermissions;
 import uk.ac.tees.w9312536.bukolafatunde.R;
 
 public class EditProfileFragment extends Fragment {
 
     View view;
-    TextView tvEmpty;
+    EditText etName, etAboutMe;
+    CircularImageView mProfileImage;
+    Button saveButton;
+
+    /* Image request code */
+    private int PICK_IMAGE_REQUEST = 1;
+    /* storage permission code */
+    private static final int RC_STORAGE_PERM = 101;
 
 
     public EditProfileFragment() {
@@ -36,7 +54,37 @@ public class EditProfileFragment extends Fragment {
     }
 
     private void initUi() {
-       // tvEmpty = view.findViewById(R.id.tv_empty_downloads);
+        etName = view.findViewById(R.id.edit_text_profile_edit_name);
+        mProfileImage = view.findViewById(R.id.profile_image);
+        etAboutMe = view.findViewById(R.id.edit_text_profile_about_me);
+        saveButton = view.findViewById(R.id.save_profile_button);
+
+        saveButton.setOnClickListener(v -> saveToFireStore());
+        mProfileImage.setOnClickListener(v -> loadImage());
+    }
+
+    private void loadImage() {
+        // check storage and camera permission
+        String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(getContext(), perms)) {
+            // Have permission, do the thing!
+            showFileChooser();
+        } else {
+            // Ask for both permissions
+            EasyPermissions.requestPermissions((Activity) getContext(), getString(R.string.rationale_storage),
+                    RC_STORAGE_PERM, perms);
+        }
+    }
+
+    /* method to show file chooser */
+    private void showFileChooser() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE_REQUEST);
+    }
+
+    private void saveToFireStore() {
+        String name = etName.getText().toString();
+        String aboutMe = etAboutMe.getText().toString();
     }
 }
 
